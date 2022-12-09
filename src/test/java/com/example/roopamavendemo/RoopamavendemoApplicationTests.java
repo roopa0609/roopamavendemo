fd1f322d80f66e13d793b26771dbc338b1c347b5
Roopa K <roopa.konaparthi@gmail.com>
2022-12-09 20:36:05 +0530

    added mavendemo files

---
+++ src/test/java/com/example/roopamavendemo/RoopamavendemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roopamavendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoopamavendemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
